7f1731489a7954b5b121b926ce0c5df29ca3b791
Peter Reuterås <peter@reuteras.com>
2025-12-23 09:18:34 +0100

    Move usbclean

---
+++ usbclean/monitor.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+setup_leds() {
+    echo none > /sys/class/leds/led0/trigger
+    echo none > /sys/class/leds/led1/trigger
+    echo 0 > /sys/class/leds/led0/brightness
+    echo 0 > /sys/class/leds/led1/brightness
+    return 1
+}
+
+set_leds_green() {
+    echo 1 > /sys/class/leds/led0/brightness
+    echo 0 > /sys/class/leds/led1/brightness
+}
+
+set_leds_red() {
+    echo 0 > /sys/class/leds/led0/brightness
+    echo 1 > /sys/class/leds/led1/brightness
+}
+
+setup_leds
+
+while true; do
+    if [ -e /tmp/usbclean ]; then
+        nohup /usr/local/bin/usbclean.sh &
+        rm -f /tmp/usbclean
+    fi
+    if pgrep usbclean.sh > /dev/null; then
+        set_leds_red
+    else
+        set_leds_green
+    fi
+    sleep 1
+done
---
+++ usbclean/usbclean.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+DEVICE=$(dmesg | grep "Attached SCSI removable disk" | sed -e "s/.*\[//" | sed -e "s/\].*//" | tail -1)
+[ -z "$DEVICE" ] && exit 1
+
+MP=/mnt/$DEVICE
+TIMES=1
+
+if [ ! -e "/dev/$DEVICE" ]; then
+    exit 1
+fi
+
+if [ ! -d "$MP" ]; then
+    mkdir "$MP"
+fi
+
+if [ -e /dev/"$DEVICE"2 ]; then
+    # Don't handle complex disk layouts on USB
+    exit 1
+fi
+
+PART=""
+if [ -e /dev/"$DEVICE"1 ]; then
+    PART="1"
+fi
+
+if ! grep -qs "/dev/$DEVICE$PART" /proc/mounts; then
+    mount "/dev/$DEVICE$PART $MP"
+fi
+
+COUNT=0
+
+while [ "$COUNT" != "1" ]; do
+    find "$MP" -mindepth 1 -print0 | xargs -0 -n 10 rm -rf > /dev/null 2>&1
+    COUNT=$(find "$MP" -maxdepth 1 | wc -l | awk '{print $1}')
+done
+
+TYPE=$(mount | grep "$MP" | awk '{print $5}')
+if [[ $TYPE != "vfat" && $TYPE != "fuseblk" ]]; then
+    exit 1
+fi
+
+BLOCKSIZE=$(blockdev --getbsz "/dev/$DEVICE$PART")
+COUNT=$((536867840 / BLOCKSIZE))
+
+for ((i = 1; i <= TIMES; i++)); do
+    file=0
+    while true; do
+        dd if=/dev/urandom of="$MP/zero.$file" bs="$BLOCKSIZE" count="$COUNT" || break
+        file=$((file + 1))
+    done
+    sync
+    rm -f "$MP"/zero*
+done
+
+umount "$MP"
+rmdir "$MP"
